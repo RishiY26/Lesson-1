1ddd48c656a169b38c6a1bd90bf61b069f08fb09
RishiY26 <sakhetyalamanchili@gmail.com>
2024-11-09 15:12:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onresize=screen
+window.onload=screen
+function screen(){
+width=window.innerWidth
+height=window.innerHeight
+document.getElementById("size").innerHTML="Width: "+width+"\n"+"Height: "+height
+
+}
